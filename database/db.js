43417b8867eccdb3f43f174483b698434577631e
AndyM11 <andymreyes@outlook.es>
2022-05-29 14:57:31 -0400

    Proyecto Final

--- database/db.js
+++ database/db.js
@@ -1,13 +1,13 @@
 const mysql = require('mysql');
 
 const conexion = mysql.createConnection({
-    database: "login_node",
-    user: "pttxcacn56te",
+    /*database: "login_node",
+    user: "5vc5pgbaveyk",
     host: "54v3bb66nspk.us-east-3.psdb.cloud",
-    password: "pscale_pw_xTMFUDLSAnhVw8LL898dBZSVACVaCsJuTukKV-gjj5U",
+    password: "pscale_pw_05tLr87UUOJ62443Pj6sGMmvsb4UiSQd3wZ8B664SoE",
     ssl:{
         rejectUnauthorized: false
-    }
+    }*/
 });
 
 conexion.connect((error)=>{
