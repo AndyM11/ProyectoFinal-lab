b0df0c2650541417d7d3609fd292a0220b268978
AndyM11 <andymreyes@outlook.es>
2022-05-29 15:02:17 -0400

    Proyecto Final

--- index.js
+++ index.js
@@ -318,6 +318,13 @@ app.get('/logout', (req, res) => {
     });
 });
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+}
+);
+
 app.listen(PORT, (req, res) => {
     console.log(`El servidor esta escuchando por el puerto ${PORT}`);
 });
